5850a99f37bfcdbb171f5bb9329a522f8e681a0c
Nakshatra Goel <139595090+Nakshatra05@users.noreply.github.com>
2024-10-27 23:14:03 +0530

    Create grover_utils.py

---
+++ src/grover_utils.py
@@ -0,0 +1,28 @@
+from qiskit import QuantumCircuit
+
+def construct_oracle():
+    """Constructs a simple oracle for a 2x2 Sudoku puzzle."""
+    oracle = QuantumCircuit(4)
+
+    # Example constraints for a 2x2 Sudoku puzzle:
+    # Let's assume we want:
+    # - Qubit 0 and Qubit 1 to be different (representing two different cells with different values)
+    # - Qubit 2 and Qubit 3 to be different (another pair of cells with different values)
+
+    # Enforcing qubit pairs to be different values using XOR (controlled-Z gates as an example)
+    oracle.cz(0, 1)
+    oracle.cz(2, 3)
+    
+    return oracle
+
+def diffusion_operator(num_qubits):
+    """Creates the diffusion operator used in Grover's algorithm."""
+    diffusion = QuantumCircuit(num_qubits)
+    diffusion.h(range(num_qubits))
+    diffusion.x(range(num_qubits))
+    diffusion.h(num_qubits - 1)
+    diffusion.mcx(list(range(num_qubits - 1)), num_qubits - 1)
+    diffusion.h(num_qubits - 1)
+    diffusion.x(range(num_qubits))
+    diffusion.h(range(num_qubits))
+    return diffusion
